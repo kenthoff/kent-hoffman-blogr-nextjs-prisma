73ca34e9cdc5f224b2fac8aa4b0d63486731cca9
Kent Hoffman <kentjhoffman@gmail.com>
2024-10-19 21:57:08 -0400

    Fix Session issue

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,9 @@
+// pages/api/auth/[...nextauth].js
 import { NextApiHandler } from 'next';
 import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import GitHubProvider from 'next-auth/providers/github';
 import prisma from '../../../lib/prisma';
-
-const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
-export default authHandler;
 
 const options = {
   providers: [
@@ -17,3 +15,7 @@ const options = {
   adapter: PrismaAdapter(prisma),
   secret: process.env.SECRET,
 };
+
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+export default authHandler;
+export { options }; // Export the options
--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,4 +1,5 @@
-import { getSession } from 'next-auth/react';
+import { getServerSession } from "next-auth/next";
+import {options} from "../auth/[...nextauth]";
 import prisma from '../../../lib/prisma';
 
 // POST /api/post
@@ -7,7 +8,7 @@ import prisma from '../../../lib/prisma';
 export default async function handle(req, res) {
   const { title, content } = req.body;
 
-  const session = await getSession({ req });
+  const session = await getServerSession(req, res, options); // Pass the options
   const result = await prisma.post.create({
     data: {
       title: title,
